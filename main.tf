49efef2f1dd64a2ac0a7941611c32e15cea51c28
Gaurav Gunjal <gunjal.ga@northeastern.edu>
2024-10-09 21:46:34 -0400

    variables to make everything dynamic

--- main.tf
+++ main.tf
@@ -1,7 +1,67 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "infra_vpc" {
+  cidr_block = var.vpc_cidr
 
   tags = {
-    Name = "main"
+    Name = "${var.env}-vpc"
   }
+}
+
+resource "aws_internet_gateway" "infra_igw" {
+  vpc_id = aws_vpc.infra_vpc.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.infra_vpc.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.env}-public-subnet-${count.index + 1}"
+
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.infra_vpc.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+  tags = {
+    Name = "${var.env}-private-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.infra_vpc.id
+  route {
+    cidr_block = var.route_cidr_range
+    gateway_id = aws_internet_gateway.infra_igw.id
+  }
+  tags = {
+    Name = "${var.env}-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_rta_assc" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.infra_vpc.id
+  tags = {
+    Name = "${var.env}-private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private_rta_assc" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
 }
