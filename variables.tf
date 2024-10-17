edae3b6bcc09e568523aecf67680719227ff1bdf
Gaurav Gunjal <gunjal.ga@northeastern.edu>
2024-10-16 22:45:20 -0400

    Webapp instance and security group added

--- variables.tf
+++ variables.tf
@@ -37,3 +37,39 @@ variable "route_cidr_range" {
   description = "The CIDR range to route through the Internet Gateway"
   type        = string
 }
+
+variable "sec_grp_cidr" {
+  type = string
+}
+
+variable "ingress_ports" {
+  description = "List of ports to allow ingress"
+  type        = list(number)
+  default     = [22, 80, 443, 8080]
+}
+
+variable "egress_ports" {
+  description = "ports to allow egress"
+  type        = number
+  default     = 0
+}
+
+variable "AMI_id" {
+  description = "AMI id for packer image"
+  type        = string
+}
+
+variable "webapp_instance_type" {
+  description = "Type of instance for the webapp eg:t2.micro"
+  type        = string
+}
+
+variable "volume_size" {
+  description = "Volume size of the webapp instance"
+  type = number
+}
+
+variable "volume_type" {
+  description = "Volume type of the webapp instance"
+  type = string
+}
